93c6565efb01e2de8ca71ae75b751a08249e8d51
Matsadura <zidanditoo1@gmail.com>
2024-08-05 12:58:48 +0100

    [UPDATE] Change const to let, fix eslint, assign to !=

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = !task2;
+    task2 = !task;
   }
 
   return [task, task2];
