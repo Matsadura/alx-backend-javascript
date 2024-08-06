c6509d415c5dcf6e33b8f85ce300e787d3fbeb71
Matsadura <zidanditoo1@gmail.com>
2024-08-07 00:50:45 +0100

    [UPDATE] Remove .js, add return

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
-import { uploadPhoto, createUser } from './utils.js'; // eslint-disable-line
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const promises = [uploadPhoto(), createUser()];
-  Promise.all(promises).then((results) => console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`))
+  return Promise.all(promises).then((results) => console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`))
     .catch(() => new Error('Signup system offline'));
 }
