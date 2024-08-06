44bc92f738b162eb7a89717495eb0efd5857c996
Matsadura <zidanditoo1@gmail.com>
2024-08-06 23:30:20 +0100

    [ADD] Task 0: Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+function getResponseFromAPI() {
+  const promise1 = new Promise((resolve, reject) => {
+    resolve('Resolved');
+  });
+  return promise1;
+}
+
+export default getResponseFromAPI;
