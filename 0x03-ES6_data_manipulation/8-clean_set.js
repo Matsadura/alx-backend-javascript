3e8d50ee8004630180b913f2413988b0ffe1561f
Matsadura <zidanditoo1@gmail.com>
2024-08-12 19:21:50 +0100

    [UPDATE] Add error handling

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,11 @@
 export default function cleanSet(set, startString) {
   let result = '';
+
+  if (startString === '' || typeof (startString) !== 'string') {
+    return result;
+  }
   for (const element of set) {
-    if (element.startsWith(startString) && startString !== '') {
+    if (element.startsWith(startString)) {
       if (result === '') {
         result = result.concat(element.slice(startString.length));
       } else {
